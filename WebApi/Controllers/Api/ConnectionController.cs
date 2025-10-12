a587a2883b8dd3e570b9297d98f7b45c1043f617
Sergey <mr.ultimateground@gmail.com>
2025-10-12 16:07:20 +0200

    Api created

---
+++ WebApi/Controllers/Api/ConnectionController.cs
@@ -0,0 +1,40 @@
+﻿using Bll.Dto;
+using Bll.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionController : ControllerBase
+    {
+        private readonly IChemicalCalculatorService _service;
+        public ConnectionController(IChemicalCalculatorService service)
+        {
+            _service = service;
+        }
+
+        [Route("{AtomicNumber1}/{AtomicNumber2}")]
+        [HttpGet]
+        public async Task<IActionResult> Get(int AtomicNumber1,int AtomicNumber2)
+        {
+            try
+            {
+                return Ok(await _service.CalculateConnection(AtomicNumber1, AtomicNumber2));
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/Api/ElementsController.cs
@@ -0,0 +1,51 @@
+﻿using Bll.Services;
+using Bll.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace WebApi.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElementsController : ControllerBase
+    {
+        private readonly IChemicalCalculatorService _service;
+        public ElementsController(IChemicalCalculatorService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await _service.GetChemicalElements());
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+        [Route("{AtomicNumber}")]
+        [HttpGet]
+        public async Task<IActionResult> Get(int AtomicNumber)
+        {
+            try
+            {
+                return Ok(await _service.GetChemicalElementByAtomicNumber(AtomicNumber));
+            }
+            catch (InvalidOperationException)
+            {
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
